d176bc1f4b5192ea3797f871d7f75e9cd7543569
abelsharman <tleksuerbasov@mail.ru>
2022-10-28 17:24:08 +0600

    feat(app): added news, new, publications pages

---
+++ components/atoms/PublicationCard/index.vue
@@ -0,0 +1,48 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <div class="p_card">
+    <img
+      loading="lazy"
+      src="@/assets/img/publication_card.png"
+      alt="publication_card"
+    />
+    <div>
+      <img
+        loading="lazy"
+        src="@/assets/icons/pdf.svg"
+        alt="pdf"
+        width="21"
+        height="28"
+      />
+      <p>Отчет о корпоративной ответственности за 2019-20 гг</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.p_card {
+  img {
+    width: 100%;
+  }
+  div {
+    display: grid;
+    grid-template-columns: 21px 1fr;
+    margin-top: 12px;
+    p {
+      font-family: 'Roboto', sans-serif;
+      padding-left: 12px;
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 22px;
+      color: #30454e;
+      @media (orientation: portrait) {
+        font-size: 14px;
+        line-height: 18px;
+      }
+    }
+  }
+}
+</style>
---
+++ components/atoms/Tag/index.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+    },
+    selected: {
+      type: Boolean,
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="tag" :class="{ tag_selected: selected }">
+    <span v-text="text"></span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.tag {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  color: #015467;
+  padding: 6px 10px;
+  border: 1px solid #015467;
+  border-radius: 4px;
+  display: inline-block;
+  margin-right: 8px;
+  margin-bottom: 8px;
+  @media (orientation: portrait) {
+    border: 1px solid #30454e;
+    color: #30454e;
+    font-size: 14px;
+    line-height: 18px;
+  }
+  &_selected {
+    background: #015467;
+    color: #fff;
+  }
+}
+</style>
