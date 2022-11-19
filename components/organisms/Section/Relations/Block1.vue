a94ade111cb7b4dfe816640dd1dca58e34d0d243
BeibarysMaksatuly <beibarys.m.03@gmail.com>
2022-11-19 15:05:09 +0600

    relations done

---
+++ components/organisms/Section/Relations/Block1.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="connect">
+    <div class="container">
+      <AtomsHeading type="h3">
+        Стратегическое планирование персонала
+      </AtomsHeading>
+      <div class="connect_content">
+        <div>
+          <p>
+            В июле 2016 г. Партнеры объявили о согласовании окончательного
+            решения по финансированию Проекта будущего расширения – Проекта
+            управления устьевым давлением (ПБР-ПУУД), что представляет собой
+            следующий этап крупного расширения добычи и производства на
+            Тенгизском месторождении нефти.
+          </p>
+          <p>
+            В рамках проекта ПБР-ПУУД нам важно сохранять прозрачную
+            двустороннюю связь с нашими кредиторами. ТШО внедряет полноценные
+            отношения с кредиторами, чтобы гарантировать, что заинтересованные
+            стороны будут иметь постоянные возможности задавать вопросы,
+            комментировать и предоставлять обратную связь.
+          </p>
+          <p>
+            Наша цель - поддерживать открытый диалог с нашей базой кредиторов и
+            своевременно реагировать на вопросы. Помимо предоставления
+            информации, которая содержится под закладкой Компания, мы будем рады
+            помочь вам, оперативно предоставив ответы на ваши вопросы.
+          </p>
+        </div>
+        <img src="@/assets/img/relations/factory.svg" alt="factory" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.connect {
+  background: #f2f6f7;
+  padding: 40px 0 80px;
+  @include tablet() {
+    padding: 20px 0 40px;
+  }
+  .container {
+    flex-direction: column;
+  }
+  h3 {
+    max-width: 776px;
+    color: #30454e;
+    margin-bottom: 40px;
+    @include tablet() {
+      margin-bottom: 20px;
+    }
+  }
+  &_content {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    @include tablet() {
+      flex-direction: column-reverse;
+      justify-content: flex-start;
+    }
+    div {
+      p {
+        max-width: 776px;
+        margin-bottom: 12px;
+        &:last-child {
+          margin-bottom: 0;
+        }
+        @include tablet() {
+          font-size: 16px;
+          line-height: 20px;
+        }
+      }
+    }
+
+    img {
+      margin-top: -132px;
+      @include tablet() {
+        width: 250.0436553955078px;
+        height: 217px;
+        margin: 0 auto;
+        margin-bottom: 20px;
+      }
+    }
+  }
+}
+</style>
---
+++ components/organisms/Section/Relations/Block3.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="creditors">
+    <div class="container">
+      <AtomsTitle class="mb40" small> Кредиторам </AtomsTitle>
+      <div class="creditors_info">
+        <div class="info_block">
+          <span>Рейтинговое агенства</span>
+          <div class="info_block__footer">
+            <span>Standard & Poors`s</span>
+            <span>Moody`s</span>
+          </div>
+        </div>
+        <div class="info_block">
+          <span>Последняя редакция</span>
+          <div class="info_block__footer">
+            <span>21/12/2021</span>
+            <span>30/06/2021</span>
+          </div>
+        </div>
+        <div class="info_block">
+          <span>Мировой рейтинг</span>
+          <div class="info_block__footer">
+            <span>BBB-</span>
+            <span>Baa2</span>
+          </div>
+        </div>
+        <div class="info_block">
+          <span>Перспектива</span>
+          <div class="info_block__footer">
+            <span>Положительный</span>
+            <span>Устойчивый</span>
+          </div>
+        </div>
+        <div class="info_block">
+          <span>Веб-страница</span>
+          <div class="info_block__footer">
+            <span>S & P</span>
+            <span>Moody`s</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.creditors {
+  width: 100%;
+  padding: 80px 0;
+  background: #f2f6f7;
+  @include tablet() {
+    padding: 40px 0;
+    .mb40 {
+      margin-bottom: 20px;
+    }
+  }
+  &_info {
+    display: flex;
+    justify-content: space-between;
+    @include tablet() {
+      flex-direction: column;
+    }
+  }
+}
+.info_block {
+  display: flex;
+  flex-direction: column;
+  @include tablet() {
+    margin-bottom: 20px;
+  }
+  span {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22px;
+    color: #30454e;
+    @include tablet() {
+      font-size: 16px;
+      line-height: 20px;
+    }
+  }
+  &__footer {
+    display: flex;
+    flex-direction: column;
+    grid-gap: 5px;
+    margin-top: 20px;
+    @include tablet() {
+      margin-top: 12px;
+    }
+    span {
+      font-weight: 300;
+    }
+  }
+}
+</style>
