3839d93a5da3b32088b8ebaa73734a6d5e201f67
Aslan <korpeevaslan@gmail.com>
2023-01-19 17:05:41 +0500

    feat: add vacancy page iframe vacancy list

--- components/templates/Vacancy/index.vue
+++ components/templates/Vacancy/index.vue
@@ -2,15 +2,15 @@
   <div class="vacancy">
     <div class="container">
       <MoleculesBreadcrumbs class="mt40 mb20">
-        <AtomsBreadOption to="/">{{
-          $t('vacancy.breadcrumbs[0]')
-        }}</AtomsBreadOption>
-        <AtomsBreadOption to="/career">{{
-          $t('vacancy.breadcrumbs[1]')
-        }}</AtomsBreadOption>
-        <AtomsBreadOption to="/career/vacancy">{{
-          $t('vacancy.breadcrumbs[2]')
-        }}</AtomsBreadOption>
+        <AtomsBreadOption to="/">
+          {{ $t('vacancy.breadcrumbs[0]') }}
+        </AtomsBreadOption>
+        <AtomsBreadOption to="/career">
+          {{ $t('vacancy.breadcrumbs[1]') }}
+        </AtomsBreadOption>
+        <AtomsBreadOption to="/career/vacancy">
+          {{ $t('vacancy.breadcrumbs[2]') }}
+        </AtomsBreadOption>
       </MoleculesBreadcrumbs>
       <AtomsTitle>{{ $t('vacancy.breadcrumbs[2]') }}</AtomsTitle>
     </div>
@@ -18,7 +18,15 @@
     <OrganismsSectionVacancyBlock2 />
     <!-- <OrganismsSectionVacancyBlockDeclaration /> -->
     <!-- <OrganismsSectionVacancyBlock3 /> -->
-    <OrganismsSectionVacancyBlock4 />
+    <!--    <OrganismsSectionVacancyBlock4 />-->
+    <div class="container">
+      <iframe
+        height="850"
+        width="100%"
+        style="border: medium none; padding-top: 20px"
+        src="https://tengizchevroil.amris.com/wizards_v2/tengiz/vacancySearch.php"
+      ></iframe>
+    </div>
     <!-- <OrganismsSectionVacancyBlock5 />
     <OrganismsSectionVacancyBlock6 /> -->
     <OrganismsSectionVacancyBlock7 />
