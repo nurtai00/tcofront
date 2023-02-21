baf48ab83385ca2006fcab11095e81fc51661012
Nurtay <44024773+nurtai00@users.noreply.github.com>
2023-02-21 11:25:38 +0500

    Update Block3.vue

--- components/organisms/Section/Main/Block3.vue
+++ components/organisms/Section/Main/Block3.vue
@@ -3,9 +3,9 @@ export default {
   computed: {
     fileLink() {
       const mapOfFileLinkUrl = {
-        ru: 'https://norsecdeltaprojects-my.sharepoint.com/personal/n_nudiyev_norsec_kz/Documents/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/TCOntent/Main%20page/2021-%D0%BE%D1%82%D1%87%D0%B5%D1%82-%D0%BE-%D0%BA%D0%BE%D1%80%D0%BF%D0%BE%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D0%BE%D0%B9-%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8.pdf?CT=1670996186909&OR=ItemsView',
-        kk: 'https://norsecdeltaprojects-my.sharepoint.com/personal/n_nudiyev_norsec_kz/Documents/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/TCOntent/Main%20page/2021-%D0%BA%D0%BE%D1%80%D0%BF%D0%BE%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D1%82%D1%96%D0%BA-%D0%B6%D0%B0%D1%83%D0%B0%D0%BF%D0%BA%D0%B5%D1%80%D1%88%D1%96%D0%BB%D1%96%D0%BA-%D1%82%D1%83%D1%80%D0%B0%D0%BB%D1%8B-%D0%B5%D1%81%D0%B5%D0%BF.pdf?CT=1670996212309&OR=ItemsView',
-        en: 'https://norsecdeltaprojects-my.sharepoint.com/personal/n_nudiyev_norsec_kz/Documents/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/TCOntent/Main%20page/2021-corporate-responsibility-report.pdf?CT=1670996277625&OR=ItemsView',
+        ru: 'https://tengizchevroil.com/docs/default-source/tcodocuments/main-page/2021-отчет-о-корпоративной-ответственности.pdf?sfvrsn=e1f2c55c_2',
+        kk: 'https://tengizchevroil.com/docs/default-source/tcodocuments/main-page/2021-корпоративтік-жауапкершілік-туралы-есеп.pdf?sfvrsn=eff2c55c_2',
+        en: 'https://tengizchevroil.com/docs/default-source/tcodocuments/main-page/2021-corporate-responsibility-report.pdf?sfvrsn=e0f2c55c_2',
       }
       return mapOfFileLinkUrl[this.$i18n.locale]
     },
